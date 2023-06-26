67c27f4dd2f5d38d0360b66c1f5c406f1ae278f8
eugene4545 <eugeneonuoha72@gmail.com>
2023-06-26 14:20:48 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+let userName = "oge";
+
+userName ? console.log(`${userName},asked`) : console.log(`Hello`);
+
+const userQuestion = console.log(`${userName}, will i meet God today`);
+
+let randomNumber = Math.floor(Math.random() * 8);
+
+console.log(randomNumber);
+
+let eightBall = '';
+
+
+
+switch (randomNumber) {
+  case 1:
+    console.log('It is certain');
+    break;
+  case 2:
+    console.log('It is decidedly so');
+    break;
+  case 3:
+    console.log('Reply hazy try again');
+    break;
+  case 4:
+    console.log('Cannot predict now');
+    break;
+  case 5:
+    console.log('Do not count on it');
+    break;
+  case 6:
+    console.log('My sources say no');
+    break;
+  case 7:
+    console.log('Outlook not so good');
+    break;
+  case 8:
+    console.log('Signs point to yes');
+    break;
+}
+console.log(eightBall);
